3e3516b77c721fb609cfcaeb56af14f0299bff85
tommy <setiantang@outlook.com>
2016-11-24 00:56:23 +0800

    更新信息提示默认模板

--- doitphp/views/errors/message.php
+++ doitphp/views/errors/message.php
@@ -5,32 +5,15 @@
 <title>提示信息</title>
 <style type="text/css">
 <!--
-body{margin:0px; text-align:center; padding:0px; background-color:#F8F8F8; font-family:Arial,Helvetica,sans-serif; font-size:12px; line-height:20px; color:#000}
-.main{padding:30px; height:260px; width:540px; margin-top:70px; margin-right:auto; margin-bottom:0px; margin-left:auto; text-align:left; background-color:#FFF; border:1px solid #CCC}
-.main .box{margin:0px; padding:0px; height:260px; width:540px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none}
-.main .box ul{margin:0px; padding:0px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none; list-style-type:none}
-.main .box ul li{margin:0px; padding:0px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none}
-.main .box ul .menu{height:25px; width:525px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none; margin-top:0px; margin-right:0px; margin-bottom:10px; margin-left:0px; font-size:14px; line-height:25px; color:#FFF; background-color:#999; padding-top:5px; padding-right:5px; padding-bottom:5px; padding-left:10px}
-.main .box ul .content{font-size:14px;height:120px; width:500px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none; background-color:#F5F5F5; margin:0px; padding:20px; color:#C00}
-.main .box ul .line{padding:0px; height:5px; width:540px; margin-top:10px; margin-right:0px; margin-bottom:0px; margin-left:0px; border-top-style:none; border-right-style:none; border-bottom-style:none; border-left-style:none; background-color:#666}
-.main .box ul .bottom{height:25px; width:530px; background-color:#999; color:#666; font-size:12px; line-height:25px; padding-top:0px; padding-right:10px; padding-bottom:0px; padding-left:0px; text-align:right; margin-top:5px; margin-right:0px; margin-bottom:0px; margin-left:0px}
-.main .box ul .content a{font-size:12px;text-decoration:none; color:#666}
-.main .box ul .content a:hover{text-decoration:underline; color:#36C}
-.main .box ul .bottom a{color:#FFF; text-decoration:none}
-.main .box ul .bottom a:hover{text-decoration:underline}
+body{font-family:"Segoe UI","Lucida Grande",Helvetica,Arial,"Microsoft YaHei",FreeSans,Arimo,"Droid Sans","wenquanyi micro hei","Hiragino Sans GB","Hiragino Sans GB W3","FontAwesome",sans-serif;font-size:14px;line-height:24px;color:#666,text-align:center;margin:0;padding:0;background-color:#f9f9f9}a{background-color:transparent;color:#0e90d2;text-decoration:none}a:active,a:hover{outline:0;color:#095f8a}.container{width:520px;margin:100px auto 0;text-align:left;background-color:#FFF;outline:30px solid #FFF}.title{width:500px;padding:5px 10px;background-color:#EEE}.content{width:480px;height:110px;padding:20px;margin-top:10px;color:#C40;background-color:#f8f8f8}
 -->
-</style></head>
+</style>
+</head>
 
 <body>
-<div class="main">
-<div class="box">
-<ul>
-<li class="menu">提示信息:</li>
-<li class="content"><?php echo $message; ?></li>
-<li class="line"></li>
-<li class="bottom"><a href="http://www.doitphp.com" target="_blank">Powered By DoitPHP</a></li>
-</ul>
-</div>
+<div class="container">
+	<div class="title">提示信息：</div>
+	<div class="content"><?php if(isset($message)){ echo $message; } ?></div>
 </div>
 </body>
 </html>
